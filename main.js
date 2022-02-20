6c81a153f8c345f59588d8ba09b56caf01f0eb5a
ji <jiyongprk@gmail.com>
2022-02-20 00:23:10 -0500

    working

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const btns = document.querySelector('button');
 const background = document.querySelector('.container');
+const textColor = document.querySelector('.text-color');
 
 const colors = ['red', 'blue', 'green', 'pink'];
 
-let colorsChoice = btns.addEventListener('click', ()=>{
-    for (let i = 0; i <= colors.length;i++){
-        return colors[i];
-    }
+btns.addEventListener('click', ()=>{
+    let randomNumber = randomNumberGen();
+
+    console.log(randomNumber);
+
+    background.style.backgroundColor = colors[randomNumber];
+    textColor.textContent = colors[randomNumber];
 });
 
 
 
 
-console.log(colors.length);
-console.log(colorsChoice);
+//generate random number of colors array length. which is 0-3;
+function randomNumberGen() {
+    return Math.floor(Math.random() * colors.length);
+}
+
